813968b774e4bb5ac00c9db15dc176198e0b7b84
pedrotero <94190524+pedrotero@users.noreply.github.com>
2024-09-19 12:59:47 -0500

    mac check & cleanup

--- runapp.sh
+++ runapp.sh
@@ -1,8 +1,6 @@
 npm --prefix ./country-info-be install
 npm --prefix ./country-info-fe install
 
-npm --prefix ./country-info-be run dev
-&
-npm --prefix ./country-info-fe run next dev
-&
+npm --prefix ./country-info-be run dev &
+npm --prefix ./country-info-fe run dev &
 wait
